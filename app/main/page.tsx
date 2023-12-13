b37383105ef1dcbe03db3608a0a3dc7ff59b0840
Rahul_Singh <koderrahul@gmail.com>
2023-12-13 09:48:41 +0530

    fifth commit

---
+++ app/main/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center w-full h-full bg-yellow-400 min-h-screen">
+        <div className="border-2 border-red-500 p-8 rounded-md">
+          <p className=" p-8 rounded-lg bg-gradient-to-br from-green-300 via-blue-500 to-purple-600">
+            banner
+          </p>
+          <p>images</p>
+
+          <p>name</p>
+          <div>post</div>
+          <div>follower</div>
+          <div>following </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
